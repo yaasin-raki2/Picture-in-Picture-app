f3788fe6059186c630e7c9422480a37865881ff7
Yassine <yassin23raki@gmail.com>
2020-09-09 17:33:48 +0100

    functionality added

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('testing');
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => { videoElement.play() }
+    } catch (error) {
+        // catch errors here
+    }
+}
+ 
+button.addEventListener('click', async () => {
+    // Desable button
+    button.disabled = true;
+    // Start Picture in Picture
+    await videoElement.requestPictureInPicture();
+    // Reset Button
+    button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
